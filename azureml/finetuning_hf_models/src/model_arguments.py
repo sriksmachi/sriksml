ac3e3930788c1b5413b4698ce48fe123a7563b48
Srikanth Machiraju <vishwanath.srikanth@gmail.com>
2024-08-14 20:10:17 +0530

    added self hosted rag notebook

--- azureml/finetuning_hf_models/src/model_arguments.py
+++ azureml/finetuning_hf_models/src/model_arguments.py
@@ -7,6 +7,6 @@ class ModelArguments:
     """
     model_name: str = field(
         metadata={"help": "Name of the pre-training language model from huggingface/models"},
-        default="google/flan-t5-small"       
+        default="microsoft/Phi-3-mini-4k-instruct"       
     )
     
